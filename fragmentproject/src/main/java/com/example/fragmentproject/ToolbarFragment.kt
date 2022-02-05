a81a209c371bc2e12d1b6081c7e47fbb42c11421
Grandelwald <artjr97@naver.com>
2022-02-05 19:04:20 +0900

    #2 TextFragment 완성

--- fragmentproject/src/main/java/com/example/fragmentproject/ToolbarFragment.kt
+++ fragmentproject/src/main/java/com/example/fragmentproject/ToolbarFragment.kt
@@ -10,7 +10,8 @@ import com.example.fragmentproject.databinding.FragmentToolbarBinding
 
 class ToolbarFragment : Fragment() {
     private var _binding : FragmentToolbarBinding? = null
-    private val binding get() = _binding!!
+    private val binding get() = _binding!! // !! (not null 어서션): 비록 null값이 들어있을지라도 null 불가능 처럼 제약 없이 쓸수 있다.
+    //컴파일 시에는 에러가 없지만 런타임때는 에러가 날수 있따.
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
