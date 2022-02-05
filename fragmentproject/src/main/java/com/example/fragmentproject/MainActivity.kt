d8a846be74f065c9fff4f172d803164419ff33d0
Grandelwald <artjr97@naver.com>
2022-02-06 01:31:29 +0900

    #2 MainActivity.kt에 ToolbarFragment.ToolbarListener 구현

--- fragmentproject/src/main/java/com/example/fragmentproject/MainActivity.kt
+++ fragmentproject/src/main/java/com/example/fragmentproject/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.fragmentproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), ToolbarFragment.ToolbarListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onBuuttonClick(position: Int, text: String) {
+    }
+
+
 }
