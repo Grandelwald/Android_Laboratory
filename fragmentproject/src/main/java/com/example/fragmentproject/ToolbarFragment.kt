cdfd53a8f573b647722556fa03685562b4c0753f
Grandelwald <artjr97@naver.com>
2022-02-05 18:31:53 +0900

    #2 ToolbarFragment 뷰바인딩 설정

--- fragmentproject/src/main/java/com/example/fragmentproject/ToolbarFragment.kt
+++ fragmentproject/src/main/java/com/example/fragmentproject/ToolbarFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fragmentproject.databinding.FragmentToolbarBinding
 
 
 class ToolbarFragment : Fragment() {
+    private var _binding : FragmentToolbarBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toolbar, container, false)
-    }
 
+        _binding = FragmentToolbarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
